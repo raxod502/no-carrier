f0f3e6c2d0d355c1aabd28f85e6407e157b9cd27
Radon Rosborough <radon.neon@gmail.com>
2017-08-13 13:43:41 -0700

    Make validation rules less restrictive

--- src/main/scala/com/getbootstrap/no_carrier/util/PositiveInt.scala
+++ src/main/scala/com/getbootstrap/no_carrier/util/PositiveInt.scala
@@ -1,5 +1,5 @@
 package com.getbootstrap.no_carrier.util
 
 object PositiveInt {
-  def unapply(int: Int): Option[Int] = if (int > 0) Some(int) else None
+  def unapply(int: Int): Option[Int] = if (int >= 0) Some(int) else None
 }
